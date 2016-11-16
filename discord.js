5052b8f56952185fc572025c9fdd9e32edb567d4
Andy Justice <Andy Justice>
2016-11-15 19:47:11 -0800

    Added Sound Files / Folder Structure

--- discord.js
+++ discord.js
@@ -1,13 +1,23 @@
 var Discord = require('discord.io');
+
+// Load Environment Variables [.env.json] for Tokens
 require('dot-env');
 
+// Import Channels, Boss Info, Discord Setup, Commands
+var channels = require('./data/channels.json');
+var bosses = require('./data/bosses.json');
+var commands = require('./data/commands.json');
+
+// Authenticate Client
 var bot = new Discord.Client({
     token: process.env.token,
     autorun: true
 });
 
+// On Bot StartUP
 bot.on('ready', function() {
     console.log(bot.username + " - (" + bot.id + ")");
+    // Send Log Data
 });
 
 bot.on('message', function(user, userID, channelID, message, event) {
